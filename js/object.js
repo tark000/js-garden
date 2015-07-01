b3781154d1e4e902509b87a09b1a3e7759a90975
Taras Grabets <TarasGrabets@Livenation.com>
2015-07-01 19:39:56 +0300

    2

--- js/object.js
+++ js/object.js
@@ -1,1 +1,19 @@
-object.js
+$(function() {
+	console.log(false.toString());
+	console.log([1,2,3].toString());
+	function Foo() {};
+	Foo.bar = 1;
+	console.log(Foo.bar);
+
+	var foo = {};
+
+	var bar = {test: 12};
+
+	var foo = {name: 'kitten'};
+	console.log(foo.name);
+	console.log(foo['name']);
+
+	var get = 'name';
+
+
+});
